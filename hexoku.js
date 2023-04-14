2293c9824860d0521b3a43616e26938ca60c62ff
Lukas Gimberis <lukasgimberis@gmail.com>
2023-04-14 11:54:44 +0100

    Move board / tile widths inside our setup function so we can adjust the board width programatically. I broke a few things with some functions called without 'this' keyword that access 'this'. Will fix in next commit by moving all functions inside the hexoku object properly

--- hexoku.js
+++ hexoku.js
@@ -1,48 +1,73 @@
 "use strict";
-const TILE_WIDTH = 3;
-const BOARD_WIDTH = TILE_WIDTH * TILE_WIDTH;
-const BOARD_SQUARES = BOARD_WIDTH * BOARD_WIDTH;
-const LETTERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] //, 'a', 'b', 'c', 'd', 'e', 'f', '0']
 
-function setup() {
+const LETTERS = {
+  3: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  4: ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', '0']
+}
+
+const CLASSES = {
+  3: {
+    'board': 'sudoku',
+    'square': 'sudoku-square',
+    'squareText': 'sudoku-square-text',
+    'clicked': 'sudoku-square sudoku-clicked',
+    'row': 'sudoku-row'
+  },
+  4: {
+    'board': 'hexoku',
+    'square': 'hexoku-square',
+    'squareText': 'hexoku-square-text',
+    'clicked': 'hexoku-square hexoku-clicked',
+    'row': 'hexoku-row'
+  }
+}
+
+function setup(width = 3) {
   // Create our board, fill it, empty it, and add relevant event listeners.
+  if (!LETTERS.hasOwnProperty(width)) {
+    return;
+  }
+  this.letters = LETTERS.width;
+  this.tileWidth = width;
+  this.boardWidth = width * width;
+  this.numSquares = this.boardWidth * this.boardWidth;
 
-  let root = document.getElementById("hexoku");
-  this.board = new Array(BOARD_SQUARES).fill('');
-  this.domBoard = new Array(BOARD_SQUARES).fill(0);
+  let root = document.getElementById(CLASSES.width.board);
+  this.board = new Array(this.numSquares).fill('');
+  this.domBoard = new Array(this.numSquares).fill(0);
   let board = document.createElement("table");
   board.addEventListener('click', this.onClickBoard.bind(this), true);
   document.addEventListener('keydown', this.onType.bind(this), true);
   root.appendChild(board);
-  for (let y = 0; y < BOARD_WIDTH; y++) {
+  for (let y = 0; y < this.boardWidth; y++) {
     let row = document.createElement("tr");
-    row.setAttribute('class', 'hexoku-row');
+    row.setAttribute('class', CLASSES.width.row);
     board.appendChild(row);
-    for (let x = 0; x < BOARD_WIDTH; x++) {
-      let column = document.createElement("td");
-      column.setAttribute('class', 'hexoku-square');
-      column.addEventListener('click', onClickSquare(y * BOARD_WIDTH + x, this).bind(column));
-      row.appendChild(column);
-      let columnTextElement = document.createElement("span");
-      columnTextElement.setAttribute('class', 'hexoku-square-text');
-      column.appendChild(columnTextElement);
-      this.domBoard[y * BOARD_WIDTH + x] = column;
+    for (let x = 0; x < this.boardWidth; x++) {
+      let square = document.createElement("td");
+      square.setAttribute('class', CLASSES.width.square);
+      square.addEventListener('click', onClickSquare(y * this.boardWidth + x, this).bind(square));
+      row.appendChild(square);
+      let squareTextElement = document.createElement("span");
+      squareTextElement.setAttribute('class', CLASSES.width.squareText);
+      square.appendChild(columnTextElement);
+      this.domBoard[y * this.boardWidth + x] = square;
     }
   }
-  this.generateSolvedBoard();
-  hideSomeSquares(this.board, 0.5);
+  this.board = this.generateSolvedBoard(width);
+  this.board = hideSomeSquares(this.board, 0.5);
   this.render();
 }
 
 function onType(event) {
   // Update the DOM and board when the user types a valid key having selected a square.
   let key = event.key.toString();
-  if (this.squareTarget >= 0 && LETTERS.includes(key)) {
+  if (this.squareTarget >= 0 && this.letters.includes(key)) {
     this.board[this.squareTarget] = key
     for (let child of this.domBoard[this.squareTarget].children) {
       child.innerText = key
     }
-    this.domBoard[this.squareTarget].setAttribute('class', 'hexoku-square');
+    this.domBoard[this.squareTarget].setAttribute('class', CLASSES[this.width].squareText);
     this.squareTarget = -1;
     this.render();
   }
@@ -50,32 +75,32 @@ function onType(event) {
 
 function onClickSquare(index, hexoku) {
   // Makes a square look "clicked". Also tag it as being the target of keyboard inputs.
-  return function(event) {
-    this.setAttribute('class', 'hexoku-square hexoku-clicked');
+  return function(_event) {
+    this.setAttribute('class', CLASSES[hexoku.width].squareClicked);
     hexoku.squareTarget = index;
   }
 }
 
-function onClickBoard(event) {
+function onClickBoard(_event) {
   // Reset the class on all squares so none appear "clicked". Reset the target so typing does nothing again.
-  for (let i = 0; i < BOARD_SQUARES; i++) {
-    this.domBoard[i].setAttribute('class', 'hexoku-square');
+  for (let i = 0; i < this.numSquares; i++) {
+    this.domBoard[i].setAttribute('class', CLASSES[this.width].square);
   }
   this.squareTarget = -1;
 }
 
 function render() {
   // Update DOM elements according to our in-memory board
-  for (let i = 0; i < BOARD_SQUARES; i++) {
+  for (let i = 0; i < this.numSquares; i++) {
     this.domBoard[i].innerText = this.board[i];
   }
-  console.log("Board is valid: ", this.isBoardValid(this.board));
+  console.log("Board is valid: ", isBoardValid(this.board));
 }
 
 function squaresInRow(board, row) {
   // Return an array of the non-empty contents of all squares in row #{row}
   let squares = []
-  for (let i = row * BOARD_WIDTH; i < (row + 1) * BOARD_WIDTH; i++) {
+  for (let i = row * this.boardWidth; i < (row + 1) * this.boardWidth; i++) {
     if (board[i]) {
       squares.push(board[i]);
     }
@@ -86,7 +111,7 @@ function squaresInRow(board, row) {
 function squaresInColumn(board, column) {
   // Return an array of the non-empty contents of all squares in column #{column}
   let squares = []
-  for (let rowIndex = 0; rowIndex < BOARD_SQUARES; rowIndex += BOARD_WIDTH) {
+  for (let rowIndex = 0; rowIndex < this.numSquares; rowIndex += this.boardWidth) {
     if (board[rowIndex + column]) {
       squares.push(board[rowIndex + column]);
     }
@@ -96,24 +121,24 @@ function squaresInColumn(board, column) {
 
 function coordToTileIndex(row, column) {
   // Convert a {row, column} combination to a "tile index".
-  return Math.floor(row / TILE_WIDTH) * TILE_WIDTH + Math.floor(column / TILE_WIDTH);
+  return Math.floor(row / this.tileWidth) * this.tileWidth + Math.floor(column / this.tileWidth);
 }
 
 function indexToTileIndex(index) {
   // Convert an array index to a {row, column} combination and return the "tile index"
-  let column = index % BOARD_WIDTH;
-  let row = (index - column) / BOARD_WIDTH;
-  return this.coordToTileIndex(row, column);
+  let column = index % this.boardWidth;
+  let row = (index - column) / this.boardWidth;
+  return coordToTileIndex(row, column);
 }
 
 function squaresInTile(board, tileIndex) {
   // Return an array of the non-empty contents of all squares in tile #{tileIndex}
   let squares = []
-  let rowStart = TILE_WIDTH * Math.floor(tileIndex / TILE_WIDTH)
-  let columnStart = TILE_WIDTH * (tileIndex % TILE_WIDTH);
-  for (let row = rowStart; row < rowStart + TILE_WIDTH; row++) {
-    let rowIndex = row * BOARD_WIDTH;
-    for (let column = columnStart; column < columnStart + TILE_WIDTH; column++) {
+  let rowStart = this.tileWidth * Math.floor(tileIndex / this.tileWidth)
+  let columnStart = this.tileWidth * (tileIndex % this.tileWidth);
+  for (let row = rowStart; row < rowStart + this.tileWidth; row++) {
+    let rowIndex = row * this.boardWidth;
+    for (let column = columnStart; column < columnStart + this.tileWidth; column++) {
       if (board[rowIndex + column]) {
         squares.push(board[rowIndex + column]);
       }
@@ -129,27 +154,27 @@ function doesArrayHaveDuplicates(array) {
 
 function isPlacementValid(board, index, value) {
   // Check if it is valid to place 'value' at square 'index'
-  let column = index % BOARD_WIDTH;
-  let row = (index - column) / BOARD_WIDTH;
+  let column = index % this.boardWidth;
+  let row = (index - column) / this.boardWidth;
 
   // Test row
-  let isRowValid = !(this.squaresInRow(board, row).includes(value));
+  let isRowValid = !(squaresInRow(board, row).includes(value));
 
   // Test column
-  let isColumnValid = !(this.squaresInColumn(board, column).includes(value));
+  let isColumnValid = !(squaresInColumn(board, column).includes(value));
 
   // Test the surrounding tile
-  let isTileValid = !(this.squaresInTile(board, this.coordToTileIndex(row, column)).includes(value));
+  let isTileValid = !(squaresInTile(board, coordToTileIndex(row, column)).includes(value));
   return isRowValid && isColumnValid && isTileValid;
 }
 
 function isBoardValid(board) {
   // Check that our board abides by the rules of Sudoku
   // Rows, columns, and tiles are all checked
-  for (let i = 0; i < BOARD_WIDTH; i++) {
-    let rowDuplicates = doesArrayHaveDuplicates(this.squaresInRow(board, i));
-    let columnDuplicates = doesArrayHaveDuplicates(this.squaresInColumn(board, i));
-    let tileDuplicates = doesArrayHaveDuplicates(this.squaresInTile(board, i));
+  for (let i = 0; i < this.boardWidth; i++) {
+    let rowDuplicates = doesArrayHaveDuplicates(squaresInRow(board, i));
+    let columnDuplicates = doesArrayHaveDuplicates(squaresInColumn(board, i));
+    let tileDuplicates = doesArrayHaveDuplicates(squaresInTile(board, i));
     if (rowDuplicates || columnDuplicates || tileDuplicates) {
       return false;
     }
@@ -159,20 +184,20 @@ function isBoardValid(board) {
 
 function generateSolvedBoard() {
   // Randomly generate a solved board
-  let board = Array(BOARD_SQUARES).fill('')
-  let history = Array(BOARD_SQUARES).fill(0);
-  history[0] = Array.from(LETTERS);
+  let board = Array(this.numSquares).fill('')
+  let history = Array(this.numSquares).fill(0);
+  history[0] = Array.from(this.letters);
 
   let square = 0;
-  while (square < BOARD_SQUARES) {
+  while (square < this.numSquares) {
     if (history[0].length == 0) {
       console.log("Could not generate any board at all. Shameful!");
       return;
     }
 
     // Reset all possible options for the next square
-    if (square < BOARD_SQUARES - 1) {
-      history[square + 1] = Array.from(LETTERS);
+    if (square < this.numSquares - 1) {
+      history[square + 1] = Array.from(this.letters);
     }
 
     let placed = false;
@@ -180,7 +205,7 @@ function generateSolvedBoard() {
       let letterIndex = Math.floor(Math.random() * history[square].length);
       let letter = history[square].splice(letterIndex, 1)[0];
 
-      if (this.isPlacementValid(board, square, letter)) {
+      if (isPlacementValid(board, square, letter)) {
         board[square] = letter;
         placed = true;
         square++;
@@ -193,29 +218,23 @@ function generateSolvedBoard() {
       square--;
     }
   }
-  this.board = board;
+  return board;
 }
 
 function hideSomeSquares(board, percentage) {
   // Replace {percentage}% of squares in board with empty space
-  let squares = Array(BOARD_SQUARES).fill(0).map((x, i) => i);
-  for (let squareNumber = 0; squareNumber < Math.floor(BOARD_SQUARES * percentage); squareNumber++) {
+  let squares = Array(this.numSquares).fill(0).map((_x, i) => i);
+  for (let squareNumber = 0; squareNumber < Math.floor(this.numSquares * percentage); squareNumber++) {
     let index = Math.floor(Math.random() * squares.length);
     let square = squares.splice(index, 1)[0];
     board[square] = '';
   }
+  return board
 }
 
 let hexoku = {
   setup: setup,
   render: render,
-  squaresInRow: squaresInRow,
-  squaresInColumn: squaresInColumn,
-  squaresInTile: squaresInTile,
-  coordToTileIndex: coordToTileIndex,
-  isBoardValid: isBoardValid,
-  isPlacementValid: isPlacementValid,
-  generateSolvedBoard: generateSolvedBoard,
   onClickBoard: onClickBoard,
   squareTarget: -1,
   onType: onType
